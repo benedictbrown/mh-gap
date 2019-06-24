f187aba0c43277ceccbbf94f0a41be1d752431ff
yaaowusu00 <49773006+yaaowusu00@users.noreply.github.com>
2019-06-24 13:11:44 -0400

    Update Form.js

--- front-end/src/components/Form.js
+++ front-end/src/components/Form.js
@@ -1,12 +1,17 @@
 // import PDFDocument from "pdfkit";
 const api = "http://localhost:3001";
-var FormData = require('form-data');
+let FormData = require('form-data');
 
 export default {
   name: "FormPage",
   data() {
+
     return {
-      step: 1,
+      json: {},
+      step: "step1",
+      path: ['step1'],
+      html: '',
+      responses: {},
       form: {
         fname: "",
         lname: "",
@@ -31,26 +36,81 @@ export default {
       }
     };
   },
+  mounted() {
+    console.log("A1");
+    console.log(this);
+    let that = this;
+    fetch('./mhgsteps.json')
+      .then(res => res.json())
+      .then(res => { console.log(JSON.stringify(res)); return res; })
+      .then(res => {
+        console.log("A2");
+        console.log(res);
+        console.log(that);
+        that.json = res;
+      });
+  },
   methods: {
-    prev(step) {
-      this.step = step;
+    recordAnswer(answer) {
+      this.responses[this.step] = answer;
+      console.log(this.responses);
     },
-    next(newStep) {
+    changeStep(newStep, json) {
+      console.log(json[this.step]['nextTrue'])
+      this.path.push(newStep)
+      console.log(this.path);
       this.step = newStep;
     },
+    previous() {
+      let last = this.path[this.path.length - 2];
+      //console.log(last)
+      this.path.pop(last);
+      delete this.responses[last];
+      console.log(this.responses)
+      if (this.step === 'step1') {
+        console.log(this.step);
+        this.step = 'step1';
+      }
+      else { this.step = last; }
+    },
+    fillHtml() {
+      if (this.step === 'step7' || 'step5') {
+        this.html = this.json[this.step]['html']
+        return true;
+      }
+    },
+    loadData() {
+      console.log(step);
+      console.log(this);
+      let that = this;
+      fetch('./mhgsteps.json')
+        .then(res => res.json())
+        .then(res => { console.log(JSON.stringify(res)); return res; })
+        .then(res => {
+          console.log(step);
+          console.log(res);
+          console.log(that);
+          that.json = res;
+        });
+    },
     submit() {
-      alert('Submitting...')
-      var formData = new FormData("FormPage");
-      fetch(`${api}/submit`, {
-        method: 'POST',
-        headers: {
-          Accept: "application/json",
-        },
-        body: formData
-      })
-      .then(res => res.json())
-      .then(res => console.log('Success:', JSON.stringify(res)))
-      .then(console.log("Submitted!"));
+      if (this.json[this.step]['next'] === "submit") {
+        alert('Submitting...')
+        var formData = new FormData("FormPage");
+        fetch(`${api}/submit`, {
+          method: 'POST',
+          headers: {
+            Accept: "application/json",
+          },
+          body: formData
+        })
+          .then(res => res.json())
+          .then(res => console.log('Success:', JSON.stringify(res)))
+          .then(console.log("Submitted!"));
+      }
+      else {
+        changeStep(json[this.step]['nextTrue'], json)
+      }
     }
   }
-};
+}
